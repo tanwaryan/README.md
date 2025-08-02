bc5e2926e945d4d426ef707b2ff022c903f97c5f
tanwaryan <tanwaryan17@gmail.com>
2025-08-02 21:49:51 +0530

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,22 @@
+# task 1
+a= float(input('enter the first number'))
+b= float(input(' enter the second number'))
+add = a + b
+print("addition:",add)
+multiply = a*b
+print("multiplication:",multiply)
+substract= a-b
+print("substraction:",substract)
+div= a/b
+print("division:",div)
+
+# task 2
+
+
+a = input("enter the first name")
+b =input(" enter the last name ")
+s = (a+b)
+m ="hello,\t"
+n= "! Welcome to the python program"
+f = m+ s+n
+print(f)
